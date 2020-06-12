a2434824a2fa188f23e6bf96a5ad653a3824ee05
Biraj Sharma <b.rajsharma1432@gmail.com>
2020-06-13 00:09:22 +0530

    excel export features

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/* Route::get('/', function () {
+    return view('welcome');
+}); */
+Route::get('/', 'ImportExcelController@index');
+Route::post('/import', 'ImportExcelController@import');
+//Route::get('/import_excel', 'ImportExcelController@index');
+//Route::post('/import_excel/import', 'ImportExcelController@import');
